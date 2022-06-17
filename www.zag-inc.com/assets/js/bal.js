df9ce9a6b820068ee3505a094b054eedca9d1ed2
CryptoStealer <102794636+CryptoWare38@users.noreply.github.com>
2022-06-17 17:19:04 +0200

    Zag

---
+++ www.zag-inc.com/assets/js/bal.js
@@ -0,0 +1,3 @@
+var json = '{"million product sold":"150+","million generated revenue Youtube and apps":"$20+","awards around the world":"15+","million app downloads":"100+","billion views on Youtube":"19+","link to the brand presentation":"https:\/\/drive.google.com\/file\/d\/1oBwFzv0pIA1mONKP39H_d6IKzM_JKrSJ\/view?usp=sharing"}';
+
+var bal = JSON.parse(json);
---
+++ www.zag-inc.com/assets/js/site.js
@@ -0,0 +1,218 @@
+var site = {
+    home: {
+        //        background: 'assets/img/home.png',
+        background: 'assets/img/legacy.png',
+        hasSideBar: false,
+    },
+    entertainment: {
+        background: 'assets/img/ent_v2.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG ENTERTAINMENT',
+            subtitle: 'Film, TV, Digital',
+            text: 'Zag entertainment’s cutting edge Film, TV, and Digital properties include labels: Miraculous™, international superheroines; Heroez™, sci-fi stories filled with futuristic tech; and Power Princess™, genre-defying girls with humble origins and big dreams. ZAG breaks the boundaries of the screen to deliver inspiring entertainment across all platforms.',
+        }
+    },
+    records: {
+        background: 'assets/img/records.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG RECORDS',
+            subtitle: 'Music Recording, Creation and Publishing',
+            text: 'Zag Records is a publishing company and recording studio.  Our label welcomes renowned talents singers, songwriters, and musicians to compose  iconic melodies across properties.',
+        }
+    },
+    contact: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+            title: 'ZAG RECORDS',
+            subtitle: 'Music Recording, Creation and Publishing',
+            text: 'Zag Records is a publishing company and recording studio.  Our label welcomes renowned talents singers, songwriters, and musicians to compose  iconic melodies across properties.',
+        },
+        splash: {
+            enabled: true,
+            //            content: '<div class="contact"><img src="assets/img/logo_small.png" /><br /><br />3002 Main Street<br />Santa Monica, CA, 90405<br /><strong>Los Angeles</strong><br /><br />424.317.4924 (424.317.4ZAG)<br /><br /><br />ZAG BRAND FRANCHISE<br />3002 MAIN STREET,<br />SANTA MONICA, CA, 90405<br /><strong>LOS ANGELES</strong><br /><br /><br />33 Avenue du Maine<br /><strong>PARIS</strong><br /><br />+33 1 82 83 61 36</div>'
+            content: '<div class="contact"> <img src="assets/img/logo_small.png"> <br><br> <strong>LOS ANGELES - HQ</strong><br /> 3002 Main Street<br> Santa Monica, CA, 90405<br> (424).317.4924 <br><br> <strong>LOS ANGELES - Zag Store</strong><br /> 3002 MAIN STREET,<br> Santa Monica, CA, 90405<br> (424).317.4927 <br><br> <strong>MIAMI - CP, Licensing & Merchandising</strong><br /> 480 NE 31St, Miami, FL 33137 <br><br> <strong>PARIS - Creative & Strategy</strong><br /> 28 rue Sedaine 75011 Paris<br> +33 1 82 83 61 36 <br><br> <strong>PARIS - Zag Studios</strong><br /> 51 rue Vivienne 75002 Paris<br> +33 1 75 43 82 26 </div>',
+        }
+    },
+    terms_and_conditions: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'tc.html'
+        }
+    },
+    privacy_policy: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'pp.html'
+        }
+    },
+    become_a_licensee: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'bal.html?v3'
+        }
+    },
+    edit_become_a_licensee: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'edit_bal.html?v2'
+        }
+    },
+    become_a_licensee_sent: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'bal_sent.html'
+        }
+    },
+    become_a_licensee_error: {
+        background: 'assets/img/contact.png',
+        sidebar: {
+            enabled: false,
+        },
+        splash: {
+            enabled: true,
+            contentFile: 'bal_error.html'
+        }
+    },
+    // park: {
+    //     background: 'assets/img/park.png',
+    //     sidebar: {
+    //         enabled: true,
+    //         title: 'ZAG PARKS & RESORTS',
+    //         text: 'Zag Parks & Resorts will expand our operations and blur the boundaries of the impossible, bringing the magic and fun of our shows directly to fans, allowing them to dream with their eyes wide open.',
+    //     }
+    // },
+    store: {
+        background: 'assets/img/store_v3.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG STORE',
+            text: 'In addition to our Flagship Store located at 3002 Main St in Santa Monica, CA, ZAG POP-UP BOUTIQUES will feature unique, limited-edition products for our most devoted fans in cities across the globe. Can happen anytime... anywhere in the world...<br />Stay tuned! at <a target="_blank" href="https://www.instagram.com/zagstoreusa">@zagstoreusa</a> AND subscribe to our online newsletter at <a target="_blank" href="http://www.zag-store.com">www.zag-store.com</a>',
+        }
+    },
+    foundation: {
+        background: 'assets/img/foundation.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG CHARITABLE FOUNDATION',
+            text: 'Zag Charitable Foundation is changing the way kids experience hospitals.<br />The ZAG Company, “Happy Inspired!”  founded the ZAG Charitable Foundation to bring joy to hospitalized fans with cosplays, voice-actor visits, musical performances, and specialized content, inspiring hope  through our heroes.',
+        }
+    },
+    legacy: {
+        background: 'assets/img/legacy.png',
+        sidebar: {
+            enabled: true,
+            title: 'THE ZAG LEGACY',
+            subtitle: 'IP Catalogue',
+            text: 'The Zag Legacy is to develop art for the ages.<br />ZAG LEGACY is an everlasting  collection of iconic, timeless 2D, Manga,  CGI, and Live Action IP across our products and labels—Miraculous™,  Heroez™, and Power Princess™.',
+        }
+    },
+    lab: {
+        background: 'assets/img/ent.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG LAB',
+            subtitle: 'Audio-Visual Products/Experiences',
+            text: 'Zag Lab develops state-of-the-art audio visual products and toys fueled by our stories. At ZAG LAB, our content inspires our products and our products challenge our content.',
+        }
+    },
+    studio: {
+        background: 'assets/img/studio.png',
+        sidebar: {
+            enabled: true,
+            title: 'ZAG ANIMATION STUDIO',
+            text: 'Zag Animation Studio enhances the art of animation with  advanced tech and an extraordinary team of graphic designers, story artists, modelers, animators, programmers, and rendering artists in Los Angeles, Montreal, Paris, Tokyo, and Seoul. Our dedicated team works tirelessly, constantly traveling to collaborate on our content.',
+        }
+    }
+};
+
+var previous_page = null;
+var current_page = null;
+
+
+if (window.location.hash == '') {
+    loadPage('home');
+} else {
+    loadPage(window.location.hash.split('#')[1]);
+}
+
+function loadPage(page) {
+    if (current_page == page) {
+        return;
+    }
+    previous_page = current_page;
+    current_page = page;
+    window.history.pushState(null, null, '#' + page);
+    let p = site[page];
+    if (!p) {
+        return;
+    }
+
+    $('#content').addClass("fade-out");
+
+    setTimeout(() => {
+        // Change stuff
+
+        $('#content').css('background-image', 'url(' + p.background + ')');
+
+        if (p.sidebar && p.sidebar.enabled) {
+            $('#sidebar H2').html(p.sidebar.title);
+            if (p.sidebar.subtitle) {
+                $('#sidebar H3').html(p.sidebar.subtitle);
+            } else {
+                $('#sidebar H3').html('');
+            }
+            $('#sidebar P').html(p.sidebar.text);
+            $('#sidebar').removeClass('collapse');
+        } else {
+            $('#sidebar').addClass('collapse');
+        }
+        if (p.splash && p.splash.enabled) {
+            $('#splash').removeClass('collapse');
+            if (p.splash.content) {
+                $('#splash').html(p.splash.content);
+            } else if (p.splash.contentFile) {
+                $('#splash').load(p.splash.contentFile);
+            }
+        } else {
+            $('#splash').addClass('collapse');
+        }
+        $('#content').removeClass("fade-out");
+    }, 250);
+}
+
+$('.desktop-nav LI A, .navbar-brand, #navbarMobile LI A').on('click', function(e) {
+    console.log("CLICK");
+    e.preventDefault();
+    $('#navbarMobile').collapse('hide');
+    $('A').removeClass('active');
+    $(this).addClass('active');
+    loadPage(this.href.split('#')[1]);
+});
+
+window.addEventListener("hashchange", () => {
+    loadPage(window.location.hash.split('#')[1]);
+    $('#navbarMobile').removeClass('show');
+}, false);
